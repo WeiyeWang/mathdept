d4d5a6f913360f792c611ae910a631ef87a0ae7a
weiye.wang <kj_wangweiye@126.com>
2022-06-11 10:49:42 +0800

    20200611 1049

--- 文本处理程序等/文本整理.py
+++ 文本处理程序等/文本整理.py
@@ -1,6 +1,5 @@
 import os,re
 #os.chdir(r"D:\mathdept\文本处理程序等")
-
 with open("textfile.txt", "r", encoding = "utf8") as textfile:
     data = textfile.read()
 
@@ -18,6 +17,9 @@ def refine_brackets(matchobj):
 def boldsymbols(matchobj):
     return "\\i"+matchobj.group(1)[:-1]+"\\mathbf{"+matchobj.group(1)[-1]+"}"
 
+def insert_a_blank(matchobj):
+    return matchobj.group(1)[:-1]+" "+matchobj.group(1)[-1]
+
 data = re.sub("([\d]+. )",r"\\item ",data)
 data = re.sub("\\\\\[",r"$",data)
 data = re.sub("\\\\\]",r"$",data)
@@ -33,9 +35,13 @@ data = re.sub("“[\n]*","``",data)
 data = re.sub("”[\n]*","''",data)
 
 data = re.sub("   [ ]+",r"\\blank{50}",data)
+data = re.sub("__[_]+",r"\\blank{50}",data)
+
 
 for i in range(10):
     data = re.sub("(\{\\\\[\w]+[ ]*\}|\{\}|\{\w\})",refine_brackets,data)
+
+data = re.sub("(\\\\overrightarrow[\w])",insert_a_blank,data)
 
 data = data.replace("\\bigc","\\c")
 data = re.sub("\\\i(n[ ]+[R|Q|Z|N|C])",boldsymbols,data)
