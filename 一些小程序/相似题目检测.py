979c6a3ecd98532c18f85a0ae5df7cfdfbd3c8f8
weiye.wang <kj_wangweiye@126.com>
2022-07-27 07:12:13 +0800

    20220727 morning 标注了直至9425的相似题目

--- 一些小程序/相似题目检测.py
+++ 一些小程序/相似题目检测.py
@@ -23,7 +23,7 @@ for filename in vault_files:
 #读入全部题库数据
 
 # 重要!!!最初的新题的id
-starting_ID = 5861
+starting_ID = 7680
 
 problem_list = [[x[0],trimpic(trim(x[1]))] for x in re.findall("<BID>\\n([\s\S]*?)\\n<EID>[\s\S]*?<B题目>([\s\S]*?)<E题目>",problems)]
 #生成题号列表
@@ -37,7 +37,7 @@ for i in range(len(new_problems)):
         print(i)
     for j in range(len(old_problems)):
         similar_rate = get_equal_rate_1(new_problems[i][1],old_problems[j][1])
-        if similar_rate>0.92:
+        if similar_rate>0.90:
             alike_problems += (("%.4f" %similar_rate)+"\n\n"+new_problems[i][0]+"\t"+new_problems[i][1]+"\n\n"+old_problems[j][0]+"\t"+old_problems[j][1]+"\n\n")
 
 for i in range(len(new_problems)):
@@ -46,7 +46,7 @@ for i in range(len(new_problems)):
     for j in range(i+1,len(new_problems)):
         similar_rate = get_equal_rate_1(new_problems[i][1],new_problems[j][1])
         #print(similar_rate)
-        if similar_rate>0.92:
+        if similar_rate>0.90:
             alike_problems += (("%.4f" %similar_rate)+"\n\n"+new_problems[i][0]+"\t"+new_problems[i][1]+"\n\n"+new_problems[j][0]+"\t"+new_problems[j][1]+"\n\n")
 
 
