5465491d8de5a779b9e78548b3d34d3be7b16625
weiye.wang <kj_wangweiye@126.com>
2022-08-07 14:06:00 +0800

    20220807 更新了汇总程序, 现在能统计目标课时单元对应的题目了

--- 单元目标叙写/汇总.py
+++ 单元目标叙写/汇总.py
@@ -28,6 +28,45 @@ for f in filelist_lessons:
         page_end = trim(re.findall(r"<终止页码>([\s\S]+?)\[E课时\]",l)[0])
         lessons.append((unit_sort,lesson_sort,page_start,page_end))
 
+#统计每个目标, 每个课时, 每个单元对应的题目数量及题目
+oid = re.findall(r"K[\d]{7}[BX]","".join(objects))
+objects_id = {}
+units_id = {}
+lessons_id = {}
+for o in oid:
+    unit = o[:3]
+    lesson = o[:5]
+    if o in objects_id:
+        objects_id[o][0] += 1
+    else:
+        objects_id[o] = [1,0,""]
+    if unit in units_id:
+        units_id[unit][0] += 1
+    else:
+        units_id[unit] = [1,0,""]
+    if lesson in lessons_id:
+        lessons_id[lesson][0] += 1
+    else:
+        lessons_id[lesson] = [1,0,""]
+
+vault_list = [f for f in os.listdir("../题库0.2") if "题库" in f or "vault" in f]
+for v in vault_list:
+    with open("../题库0.2/"+v,"r",encoding = "utf8") as f:
+        data = f.read()
+        problems = re.findall("(\[B题目\][\s\S]*?\[E题目\])",data)
+        for problem in problems:
+            p_id = trim(re.findall(r"<BID>([\s\S]*?)<EID>",problem)[0])
+            p_objs = [obj for obj in trim(re.findall(r"<B目标>([\s\S]*?)<E目标>",problem)[0]).split("\n") if len(obj)>0]
+            for p_obj in p_objs:
+                objects_id[p_obj][1] += 1
+                objects_id[p_obj][2] += p_id+", "
+                if not p_id in lessons_id[p_obj[:5]][2]:
+                    lessons_id[p_obj[:5]][1] += 1
+                    lessons_id[p_obj[:5]][2] += p_id+", "
+                if not p_id in units_id[p_obj[:3]][2]:
+                    units_id[p_obj[:3]][1] += 1
+                    units_id[p_obj[:3]][2] += p_id+", "
+
 output_string = ""
 for l in lessons:
     (unit_sort,lesson_sort,page_start,page_end) = l
@@ -39,8 +78,27 @@ for l in lessons:
             index1 = trim(re.findall(r"<目标编码>([\s\S]*?)<目标内容",o)[0])
             index2 = trim(re.findall(r"<对应单元目标编码>([\s\S]*?)<叙写",o)[0])
             content = trim(re.findall(r"<目标内容>([\s\S]*?)<对应",o)[0])
-            output_string += r"\item " + index1 + "|" + index2 + "|" + content + "\n"
+            output_string += r"\item " + index1 + "|" + index2 + "|" + content + "\n\n"
+            output_string += r"关联题目数量: " + str(objects_id[index1][1]) + r". 关联题目id: " + objects_id[index1][2][:-2] + "\n\n"
+    output_string += r"\item 本课时题目数量: " + str(lessons_id[lesson_object_index][1]) + r". 关联题目id: " + lessons_id[lesson_object_index][2][:-2] + "\n\n"
     output_string += r"\end{itemize}"+"\n\n"
+
+objects_text = ""
+for id in objects_id:
+    objects_text += id + "|目标数: " + str(objects_id[id][0]) +  "|题目数: " + str(objects_id[id][1]) + "|对应题目: " + str(objects_id[id][2][:-2]) + "\n" 
+lessons_text = ""
+for id in lessons_id:
+    lessons_text += id + "|目标数: " + str(lessons_id[id][0]) +  "|题目数: " + str(lessons_id[id][1]) + "|对应题目: " + str(lessons_id[id][2][:-2]) + "\n" 
+units_text = ""
+for id in units_id:
+    units_text += id + "|目标数: " + str(units_id[id][0]) +  "|题目数: " + str(units_id[id][1]) + "|对应题目: " + str(units_id[id][2][:-2]) + "\n" 
+
+with open("题库统计/目标题目汇总.txt","w",encoding = "utf8") as f:
+    f.write(objects_text)
+with open("题库统计/课时题目汇总.txt","w",encoding = "utf8") as f:
+    f.write(lessons_text) 
+with open("题库统计/单元题目汇总.txt","w",encoding = "utf8") as f:
+    f.write(units_text) 
 
 with open("课时划分/课时划分汇总.tex","w",encoding = "utf8") as f:
     f.write(output_string)   
@@ -58,6 +116,10 @@ for obj in objects:
     texfile_string += obj[0] + " & " + obj[1] + " & " + obj[2] + r"\\ \hline" +"\n"
 with open("课时目标/课时目标汇总.tex","w",encoding="utf8") as output_file:
     output_file.write(texfile_string)
+
+
+
+
 
 os.chdir("课时目标/课时目标汇总")
 os.system("xelatex 课时目标.tex")
