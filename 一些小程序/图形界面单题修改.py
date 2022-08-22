8460c3535ab002f69cf4f0847fb42d753350e83a
weiye.wang <kj_wangweiye@126.com>
2022-08-22 13:12:34 +0800

    20220822 noon

--- 一些小程序/图形界面单题修改.py
+++ 一些小程序/图形界面单题修改.py
@@ -7,7 +7,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("测试")
-        self.resize(1000,700)
+        self.resize(1000,1000)
 
         self.button1 = QPushButton("读取题目", self)
         self.button1.move(0, 0)
@@ -17,43 +17,28 @@ class MainWindow(QWidget):
         self.button2.move(520, 0)
         self.button2.clicked.connect(self.modify_problem)
 
-        self.contentbrowser = QTextBrowser(self)
-        self.contentbrowser.move(0, 50)
-        self.contentbrowser.resize(480,150)
-        self.contentbrowser.setFont(QFont("Consolas"))
+        self.textbrowser = QTextBrowser(self)
+        self.textbrowser.move(0, 50)
+        self.textbrowser.resize(480,500)
+        self.textbrowser.setFont(QFont("Consolas"))
 
 
-        self.contentedit = QTextEdit(self)
-        self.contentedit.move(520,50)
-        self.contentedit.resize(480,150)
-        self.contentedit.setFont(QFont("Consolas"))
-        self.contentedit.textChanged.connect(self.coloringcontent)
-
-        self.anslabel1 = QLabel("答案",self)
-        self.anslabel1.move(0,200)
-        self.anslabel1.resize(50,20)
-        self.anslabel2 = QLabel("答案",self)
-        self.anslabel2.move(520,200)
-        self.anslabel2.resize(520,20)
-
-        self.answerbrowser = QTextBrowser(self)
-        self.answerbrowser.move(0, 220)
-        self.answerbrowser.resize(480,40)
-        self.answerbrowser.setFont(QFont("Consolas"))
-
-        self.answeredit = QTextEdit(self)
-        self.answeredit.move(520,220)
-        self.answeredit.resize(480,40)
-        self.answeredit.setFont(QFont("Consolas"))
-        self.answeredit.textChanged.connect(self.coloringanswer)
-
-
+        self.textedit = QTextEdit(self)
+        self.textedit.move(520,50)
+        self.textedit.resize(480,500)
+        self.textedit.setFont(QFont("Consolas"))
+        self.textedit.textChanged.connect(self.seeking_diff)
 
 
         self.id_input = QTextEdit(self)
         self.id_input.move(200,0)
         self.id_input.resize(100,40)
         self.id_input.setFont(QFont("Consolas"))
+
+        self.diff_label = QTextBrowser(self)
+        self.diff_label.move(0,550)
+        self.diff_label.resize(1000,150)
+        self.diff_label.setFont(QFont("Consolas"))
 
         dir = os.getcwd()
         if dir[-5:] == "一些小程序":
@@ -64,24 +49,17 @@ class MainWindow(QWidget):
 
     def read_problem(self):
         try:
+            self.textbrowser.setText(vt.create_string_from_dict(self.problems_dict[str(self.id_input.toPlainText()).zfill(6)]))
+            self.textedit.setText(self.textbrowser.toPlainText())
             self.problem = self.problems_dict[str(self.id_input.toPlainText()).zfill(6)]
             self.raw_problem = self.problems_list[str(self.id_input.toPlainText()).zfill(6)]
-            self.contentbrowser.setText(self.problem["content"])
-            self.contentedit.setText(self.contentbrowser.toPlainText())
-            self.answerbrowser.setText(self.problem["ans"])
-            self.answeredit.setText(self.answerbrowser.toPlainText())
-            
         except:
             print(self.id_input.toPlainText(),"有误")
         self.id = str(self.id_input.toPlainText()).zfill(6)
         print(self.id)
 
     def modify_problem(self):
-        self.problem["content"] = vt.trim(self.contentedit.toPlainText())
-        self.problem["ans"] = vt.trim(self.answeredit.toPlainText())
-        print("题目:",self.contentedit.toPlainText())
-        print("答案:",self.answeredit.toPlainText())
-        self.new_problem = vt.create_string_from_dict(self.problem)
+        self.new_problem = self.textedit.toPlainText()
         for v in self.vaults:
             if v[1:7] <= self.id <= v[9:15]:
                 with open("题库0.2/"+v,"r",encoding="utf8") as f:
@@ -90,16 +68,24 @@ class MainWindow(QWidget):
                 with open("题库0.2/"+v,"w",encoding="utf8") as f:
                     f.write(newdata)
                 break
-    def coloringcontent(self):
-        if not vt.trim(self.contentedit.toPlainText()) == vt.trim(self.contentbrowser.toPlainText()):
-            self.contentbrowser.setStyleSheet('''QWidget{background-color:#66CCFF;}''')
-        else:
-            self.contentbrowser.setStyleSheet('''QWidget{background-color:#FFFFFF;}''')
-    def coloringanswer(self):
-        if not vt.trim(self.answeredit.toPlainText()) == vt.trim(self.answerbrowser.toPlainText()):
-            self.answerbrowser.setStyleSheet('''QWidget{background-color:#66CCFF;}''')
-        else:
-            self.answerbrowser.setStyleSheet('''QWidget{background-color:#FFFFFF;}''')
+
+    def seeking_diff(self):
+        dict1 = vt.setup_dict(self.textbrowser.toPlainText())
+        dict2 = vt.setup_dict(self.textedit.toPlainText())
+        difftext = "修改项目:\n"
+        
+        for item in dict1:
+            if dict1[item] != dict2[item]:
+                difftext += item + ":\t\t" +  str(dict1[item]) +  "|修改前v修改后|" +  str(dict2[item]) + "\n"
+                
+        self.diff_label.setText(difftext)
+
+
+    # def coloring(self):
+    #     if not vt.trim(self.textedit.toPlainText()) == vt.trim(self.textbrowser.toPlainText()):
+    #         self.textbrowser.setStyleSheet('''QWidget{background-color:#66CCFF;}''')
+    #     else:
+    #         self.textbrowser.setStyleSheet('''QWidget{background-color:#FFFFFF;}''')
 
 
         
